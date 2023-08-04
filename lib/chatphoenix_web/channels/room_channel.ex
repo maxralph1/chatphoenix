5e4bc78e823e29275257c99f1536441a9a2c8d13
Max Ralph <maxralph01@gmail.com>
2023-08-04 19:39:28 +0400

    presence

--- lib/chatphoenix_web/channels/room_channel.ex
+++ lib/chatphoenix_web/channels/room_channel.ex
@@ -1,12 +1,28 @@
 defmodule ChatphoenixWeb.RoomChannel do
   use Phoenix.Channel
+  alias ChatphoenixWeb.Presence
 
   def join("room:lobby", _message, socket) do
     {:ok, socket}
   end
 
+  def join("room:lobby", %{"name" => name}, socket) do
+    send(self(), :after_join)
+    {:ok, assign(socket, :name, name)}
+  end
+
   def join("room:" <> _private_room_id, _params, _socket) do
     {:error, %{reason: "unauthorized"}}
+  end
+
+  def handle_info(:after_join, socket) do
+    {:ok, _} =
+      Presence.track(socket, socket.assigns.name, %{
+        online_at: inspect(System.system_time(:second))
+      })
+
+    push(socket, "presence_state", Presence.list(socket))
+    {:noreply, socket}
   end
 
   def handle_in("new_msg", %{"body" => body}, socket) do
